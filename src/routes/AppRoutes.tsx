9adcbb08334ea36128af0fc0e21a0ed355ec9a2e
Vince Stephen David <stoicdavid00@gmail.com>
2025-11-12 15:22:28 +0900

    Feat: Routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import {Route, Routes} from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <Route>
+        <Routes>
+        </Routes>
+    </Route>
+  )
+}
+
+export default AppRoutes
