c02afc353cf15e082512472ff00156fdc4251f7f
Vince Stephen David <stoicdavid00@gmail.com>
2025-11-18 18:20:11 +0900

    FIX: Sidebar highlight error

--- src/common/SideBar.tsx
+++ src/common/SideBar.tsx
@@ -3,11 +3,11 @@ import icons from "../constants/icon";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 const SideBar = () => {
-  const location = useLocation()
+  const location = useLocation();
   const [isActive, setIsActive] = useState<string>("dashboard");
   const [isActiveSubMenu, setIsActiveSubMenu] = useState<string | null>(null);
   const [isSubMenu, setIsSubMenu] = useState(false);
-  const [toggleMenu, setIsToggleMenu] = useState(false)
+  const [toggleMenu, setIsToggleMenu] = useState(false);
 
   const isActiveMenu = (key: string) => {
     setIsActive(key);
@@ -20,20 +20,26 @@ const SideBar = () => {
 
   return (
     <div className={` h-svh relative ${toggleMenu ? "w-24" : " min-w-96"}`}>
-      <ul className={`border-r-2 border-gray-300 h-full flex flex-col gap-2 pt-8 w-full ${toggleMenu ? "px-4" : "px-8"}`}>
+      <ul
+        className={`border-r-2 border-gray-300 h-full flex flex-col gap-2 pt-8 w-full ${
+          toggleMenu ? "px-4" : "px-8"
+        }`}
+      >
         <li className="w-full">
           <Link
             onClick={() => {
               isActiveMenu("dashboard");
             }}
-            className={`transition-all duration-300 delay-100 flex items-center  text-2xl    ${toggleMenu ? "justify-center p-1 mt-1 " : "p-2 gap-3 w-full"} ${
-              location.pathname.includes("/admin/dashboard") 
+            className={`transition-all duration-300 delay-100 flex items-center  text-2xl    ${
+              toggleMenu ? "justify-center p-1 mt-1 " : "p-2 gap-3 w-full"
+            } ${
+              location.pathname.includes("/admin/dashboard")
                 ? "menu-bg text-white rounded-md "
                 : "text-gray-400"
             }`}
             to="dashboard"
           >
-            <icons.dashboard/>
+            <icons.dashboard />
             {toggleMenu ? "" : "OverView"}
           </Link>
         </li>
@@ -43,8 +49,10 @@ const SideBar = () => {
             onClick={() => {
               isActiveMenu("availability");
             }}
-            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${toggleMenu ? "justify-center p-1 mt-1" : "p-2"} ${
-              location.pathname.includes("/admin/availability") 
+            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${
+              toggleMenu ? "justify-center p-1 mt-1" : "p-2"
+            } ${
+              location.pathname.includes("/admin/availability")
                 ? "menu-bg text-white  rounded-md"
                 : "text-gray-400"
             }`}
@@ -52,7 +60,6 @@ const SideBar = () => {
           >
             <icons.availability />
             {toggleMenu ? "" : "Availability"}
-            
           </Link>
         </li>
         <li className="">
@@ -60,8 +67,10 @@ const SideBar = () => {
             onClick={() => {
               isActiveMenu("bookings");
             }}
-            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${toggleMenu ? "justify-center p-1 mt-1" : "p-2"} ${
-              location.pathname.includes("/admin/bookings") 
+            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${
+              toggleMenu ? "justify-center p-1 mt-1" : "p-2"
+            } ${
+              location.pathname.includes("/admin/bookings")
                 ? "menu-bg text-white  rounded-md"
                 : "text-gray-400"
             }`}
@@ -76,28 +85,28 @@ const SideBar = () => {
             onClick={() => {
               isActiveMenu("renterhistory");
             }}
-            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${toggleMenu ? "justify-center p-1 mt-1" : "p-2"} ${
-              location.pathname.includes("/admin/renterhistory") 
+            className={`transition-all duration-300 delay-100 flex items-center text-2xl gap-3 ${
+              toggleMenu ? "justify-center p-1 mt-1" : "p-2"
+            } ${
+              location.pathname.includes("/admin/renterhistory")
                 ? "menu-bg text-white  rounded-md"
                 : "text-gray-400"
             }`}
-            
             to="/admin/renterhistory"
           >
-             
             <icons.booking />
             {toggleMenu ? "" : "Renter History"}
           </Link>
-           
         </li>
         <li className="">
-          <button
+          <Link to="/admin/availablevehicles"
             onClick={() => {
               isActiveMenu("vehicle");
               setIsSubMenu(true);
             }}
-
-            className={`transition-all duration-300 delay-100 cursor-pointer flex items-center text-2xl gap-3 w-full ${toggleMenu ? "justify-center p-1 mt-1" : "p-2"} ${
+            className={`transition-all duration-300 delay-100 cursor-pointer flex items-center text-2xl gap-3 w-full ${
+              toggleMenu ? "justify-center p-1 mt-1" : "p-2"
+            } ${
               isActive === "vehicle"
                 ? "menu-bg text-white  rounded-md"
                 : "text-gray-400"
@@ -105,7 +114,7 @@ const SideBar = () => {
           >
             <icons.vehicle />
             {toggleMenu ? "" : "Vehicles"}
-          </button>
+          </Link>
           {isSubMenu && (
             <ul className="pt-2 ">
               <li className={`${toggleMenu ? "px-2" : ""}`}>
@@ -140,9 +149,12 @@ const SideBar = () => {
               </li>
             </ul>
           )}
-          <button className="cursor-pointer absolute -right-2 text-violet-500 text-3xl" onClick={() => setIsToggleMenu(prev => !prev)}>
-            {toggleMenu ? <icons.toggleRight  /> : <icons.toggleLeft  />}
-            </button>
+          <button
+            className="cursor-pointer absolute -right-2 text-violet-500 text-3xl"
+            onClick={() => setIsToggleMenu((prev) => !prev)}
+          >
+            {toggleMenu ? <icons.toggleRight /> : <icons.toggleLeft />}
+          </button>
         </li>
       </ul>
     </div>
